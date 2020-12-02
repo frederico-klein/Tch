e4a6e4ff548b44ec13cde3730a6caa2b0b7bf61a
Frederico Belmonte Klein <frederico.klein@plymouth.ac.uk>
2020-12-02 15:24:57 +0000

    added switch between docker version thingies. useless for newer docker versions

--- startme.sh
+++ startme.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 # nvidia-docker became docker --gpus all now and probably the NV_GPU flag doesn't work anymore. But maybe someone might want to still use nvidia-docker2 package, so this script needs to become slightly more generic. 
+DOCKERAPIVERSION=`docker version --format '{{.Client.APIVersion}}'`
+NEWERDOCKERAPI=`echo "$DOCKERAPIVERSION >= 1.49" | bc -l` #use bc so we can do floating point stuff 
 
+#DRYRUN=yes ###comment to actually run this
+#REBUILD=yes
 
 PASSWD=$1
-MYUSERNAME=frederico
-DOCKERHOSTNAME=poop
+MYUSERNAME=$USER #frederico
+DOCKERHOSTNAME=`hostname` #poop
 THISVOLUMENAME=sshvolume-torch
 DOCKERMACHINEIP=172.28.5.30
 DOCKERMACHINENAME=tch
@@ -26,8 +30,12 @@ else
     #echo "doing nothing"
     OLDDIR=$PWD
     cd $BUILDINDIR
-    nvidia-docker build -t $DOCKERMACHINENAME $DOCKERFILE
-    #nvidia-docker build --no-cache -t $DOCKERMACHINENAME .
+    if [ -z "$REBUILD" ]
+    then
+    	nvidia-docker build -t $DOCKERMACHINENAME $DOCKERFILE
+    else
+        nvidia-docker build --no-cache -t $DOCKERMACHINENAME .
+    fi
     cd $OLDDIR
     } ||
     {
@@ -53,7 +61,9 @@ else
   scripts/enable_forwarding_docker_host.sh
   #nvidia-docker run --rm -it -p 8888:8888 -h $MACHINEHOSTNAME --network=br0 --ip=$DOCKERMACHINEIP $DOCKERMACHINENAME #bash
   {
+  echo "creating shared workspace volume"
   docker volume create --driver vieux/sshfs   -o sshcmd=$MYUSERNAME@$DOCKERHOSTNAME:$PWD/catkin_ws -o password=$PASSWD $THISVOLUMENAME
+  echo "workspace volume created"
   } ||
   {
     echo "could not mount ssh volume. perhaps vieux is not installed?" &&
@@ -61,10 +71,29 @@ else
     break
   }
 #  nvidia-docker run --rm -it -u root -p 8888:8888 -p 222:22 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $THISVOLUMENAME:/catkin_ws -h $MACHINEHOSTNAME --network=br0 --ip=$DOCKERMACHINEIP $DOCKERMACHINENAME bash # -c "jupyter notebook --port=8888 --no-browser --ip=$DOCKERMACHINEIP --allow-root &" && bash -i
-   docker run --gpus all  --rm -it -u root -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $THISVOLUMENAME:$CATKINWSPATH -v /mnt/share:/mnt/share -h $MACHINEHOSTNAME --network=br0 --ip=$DOCKERMACHINEIP $DOCKERMACHINENAME bash # -c "jupyter notebook --port=8888 --no-browser --ip=172.28.5.4 --allow-root &" && bash -i
-   #nvidia-docker run --rm -it -u root -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $THISVOLUMENAME:$CATKINWSPATH -v /mnt/share:/mnt/share -h $MACHINEHOSTNAME --network=br0 --ip=$DOCKERMACHINEIP $DOCKERMACHINENAME bash # -c "jupyter notebook --port=8888 --no-browser --ip=172.28.5.4 --allow-root &" && bash -i
 
-
+  if [ -z "$DRYRUN" ]
+  then
+      {
+	   if (( NEWERDOCKERAPI ))
+	   then
+	     {
+	     echo "starting docker - new version"
+	     docker run --gpus all  --rm -it -u root -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $THISVOLUMENAME:$CATKINWSPATH -v /mnt/share:/mnt/share -h $MACHINEHOSTNAME --network=br0 --ip=$DOCKERMACHINEIP $DOCKERMACHINENAME bash # -c "jupyter notebook --port=8888 --no-browser --ip=172.28.5.4 --allow-root &" && bash -i
+	     }
+	   else
+	     {   
+	     echo "starting docker - old version"
+	     nvidia-docker run --rm -it -u root -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $THISVOLUMENAME:$CATKINWSPATH -v /mnt/share:/mnt/share -h $MACHINEHOSTNAME --network=br0 --ip=$DOCKERMACHINEIP $DOCKERMACHINENAME bash # -c "jupyter notebook --port=8888 --no-browser --ip=172.28.5.4 --allow-root &" && bash -i
+	     }
+	   fi
+      }
+  else
+      {
+	     echo "DRY RUN"
+	     nvidia-docker run --rm -it -u root -h $MACHINEHOSTNAME --network=br0 --ip=$DOCKERMACHINEIP $DOCKERMACHINENAME bash 
+      }
+  fi
   ## if I add this with -v I can't catkin_make it with entrypoint...
   #-v /temporal-segment-networks/catkin_ws:$PWD/catkin_ws/src
   #
